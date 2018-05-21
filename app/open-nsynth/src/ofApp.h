75fa3bf3baf9275242e1451312aeedd2ba65e663
Aaron <aaron@kimmigs.de>
2018-05-21 23:34:29 +0200

    Add usb-midi as input

--- app/open-nsynth/src/ofApp.h
+++ app/open-nsynth/src/ofApp.h
@@ -30,9 +30,10 @@ limitations under the License.
 #include "PositionScreen.h"
 #include "VolumeScreen.h"
 #include "NSynth.h"
+#include "ofxMidi.h"
 
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp, public ofxMidiListener{
 	public:
 		ofApp();
 
@@ -70,6 +71,9 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo) override;
 		void gotMessage(ofMessage msg) override;
 		void audioOut(ofSoundBuffer& buffer) override;
+		void newMidiMessage(ofxMidiMessage& eventArgs);
+		ofxMidiIn midiIn;
+		ofxMidiMessage midiMessage;
 
 	private:
 		static constexpr int OLED_I2C_ADDR = 0x3d;
